35da52ee00bf99cadd861aa7dad0f2c91996e5e3
Ana Ganfornina <anies.llamani@gmail.com>
2025-02-11 13:41:58 +0100

    test: Pasa test IsExit

--- SnowTrails/SnowTrailsTesting/UserChoiceControllerTesting.swift
+++ SnowTrails/SnowTrailsTesting/UserChoiceControllerTesting.swift
@@ -7,10 +7,39 @@
 
 import Testing
 
-struct UserChoiceControllerTesting {
-
-    @Test func <#test function name#>() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+class UserChoiceControllerTesting {
+    @Suite("isExit")
+    class IsExitTests {
+        let instance: UserChoiceController
+        init() {
+            self.instance = UserChoiceController()
+        }
+        
+    
+        @Test func inputIs3UserMenuController_ReturTrue() {
+            let menu = UserMenuController()
+            let userChoice = "3"
+            #expect(instance.isExit(input: userChoice, menu: menu) == true , "Se espera que devuelva true, por lo que debería salise del bucle")
+        }
+        
+        @Test func inputIs3MenuController_ReturTrue() {
+            let menu = MenuController()
+            let userChoice = "3"
+            #expect(instance.isExit(input: userChoice, menu: menu) == true , "Se espera que devuelva true, por lo que debería salise del bucle")
+        }
+        @Test func inputIs5AdminMenuController_ReturTrue() {
+            let repositoryExample = UsersLoader(fromData: UserData())
+            let menu = AdminMenuController(whithRepository: repositoryExample)
+            let userChoice = "5"
+            #expect(instance.isExit(input: userChoice, menu: menu) == true , "Se espera que devuelva true, por lo que debería salise del bucle")
+        }
+        @Test func inputIs1UserMenuController_ReturFalse() {
+            let menu = UserMenuController()
+            let userChoice = "1"
+            #expect(instance.isExit(input: userChoice, menu: menu) == false , "Se espera que devuelva false, por lo que debería continuar en bucle")
+        }
     }
+
+    
 
 }
